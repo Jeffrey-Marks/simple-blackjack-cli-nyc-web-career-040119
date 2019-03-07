3b0cd88bc5f208a23f7cc0a460b487e8cb7259f4
Jeff M <jkdmarks@gmail.com>
2019-03-07 14:29:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,9 +43,7 @@ def hit?(total)
   prompt_user
   input = get_user_input
   if input == "s"
-    display_card_total(total)
     compare_hands(total, rand(1..11) + rand(1..11))
-    break
   elsif input == "h"
     total += deal_card
     display_card_total(total)
